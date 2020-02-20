9e0418c73b1bb484d183f94e489c50e8b2154450
Marcin Nowicki <mnowicki@utk.edu>
2020-02-19 22:53:11 -0500

    Co-authored-by: Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -1,20 +1,29 @@
 <template>
   <Layout>
-    <div class="container-inner mx-auto my-16">
-      <h1 class="">{{ $page.project.title }}</h1>
-      <div class="">{{ $page.project.startDate }} - {{ $page.project.endDate }}</div>
-      <div class="">
-        <g-link
-          v-for="tag in $page.project.tags"
-          :to="tag.path"
-          :key="tag.id"
-          v-text="tag.title"
-        />
-      </div>
+    <article>
+      <header class="header">
+        <h1 v-html="$page.project.title"/>
+        <span class="date">{{ period }}</span>
+
+        <p>
+          {{ $page.project.summary }}
+        </p>
+
+        <div class="">
+          <g-link
+            v-for="tag in $page.project.tags"
+            :to="tag.path"
+            :key="tag.id"
+          >
+            {{ tag }}
+          </g-link>
+        </div>
+      </header>
       <div class="markdown-body" v-html="$page.project.content"/>
-      <div class="mb-8">
-        <g-link to="/projects" class="">Back to Projects</g-link>
-      </div>
+    </article>
+
+    <div>
+      <g-link to="/projects" class="">Back to Projects</g-link>
     </div>
   </Layout>
 </template>
@@ -23,13 +32,16 @@
 query Project ($path: String!) {
   project (path: $path) {
     title
-    startDate (format: "MMMM D, Y")
-    endDate (format: "MMMM D, Y")
+    startDate (format: "DD MMMM YYYY")
+    startDateRaw: startDate
+    endDate (format: "DD MMMM YYYY")
+    endDateRaw: endDate
     agency
-    content
+    content,
+    summary
     tags {
       title
-      path
+      path 
     }
   }
 }
@@ -41,10 +53,30 @@ export default {
     return {
       title: `${this.$page.project.title} - projects`
     }
+  },
+  computed: {
+    period () {
+      const { 
+        startDate,
+        startDateRaw,
+        endDate,
+        endDateRaw,
+      } = this.$page.project
+
+      return +new Date(startDateRaw) >= +new Date(endDateRaw)
+        ? startDate
+        : `${startDate} - ${endDate}`
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  .header {
+    margin-bottom: 2em;
+  }
 
+  .date {
+    font-size: 14px;
+  }
 </style>
