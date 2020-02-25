c67bb2a41c779afb04c345996cfd667ba1165628
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-02-25 01:44:20 +0100

    Add RSS in footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,6 +1,9 @@
 <template lang="html">
   <footer class="footer">
-    nowickilab.science &copy; 2020
+    nowickilab.science &copy; 2020 |
+    <a href="/rss.xml">
+      <svg class="svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path d="M14.5 0h-13C.675 0 0 .675 0 1.5v13c0 .825.675 1.5 1.5 1.5h13c.825 0 1.5-.675 1.5-1.5v-13c0-.825-.675-1.5-1.5-1.5zM4.359 12.988c-.75 0-1.359-.603-1.359-1.353a1.36 1.36 0 0 1 2.718 0c0 .75-.609 1.353-1.359 1.353zM7.772 13a4.753 4.753 0 0 0-1.397-3.381A4.74 4.74 0 0 0 3 8.219V6.263c3.713 0 6.738 3.022 6.738 6.737H7.772zm3.472 0c0-4.547-3.697-8.25-8.241-8.25V2.794c5.625 0 10.203 4.581 10.203 10.206h-1.963z" fill="#626262"/></svg> RSS
+    </a>
   </footer>
 </template>
 
@@ -14,5 +17,12 @@ export default {
     text-align: center;
     margin: 80px 0 40px;
     font-size: 14px;
+  }
+
+  .svg {
+    width: 1em;
+    height: 1em;
+    position: relative;
+    top: 1.5px;
   }
 </style>
