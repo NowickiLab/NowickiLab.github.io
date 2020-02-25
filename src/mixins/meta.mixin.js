ca579777ac062f8feeb3bf4661ca8f3be4af2b90
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-02-25 02:00:44 +0100

    Fix meta tags

--- src/mixins/meta.mixin.js
+++ src/mixins/meta.mixin.js
@@ -2,14 +2,15 @@ export default function (titleSuffix = '') {
   return {
     metaInfo() {
       const { title, summary, img } = this.$page.article
+      const strippedSummary = summary.replace(/(<([^>]+)>)/ig, '')
 
       const meta = [
         { name: 'title', content: title },
-        { name: 'description', content: summary },
+        { name: 'description', content: strippedSummary },
         { property: 'og:title', content: title },
-        { property: 'og:description', content: summary },
+        { property: 'og:description', content: strippedSummary },
         { property: 'og:type', content: 'website' },
-        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:card', content: 'summary' },
         { name: 'twitter:title', content: title },
         { name: 'twitter:description', content: summary }
       ]
