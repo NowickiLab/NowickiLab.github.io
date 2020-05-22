d2bd7dc10fbd2f05e9be85b96f7ba3efad614a3a
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-05-23 00:43:05 +0200

    Remove google form way to create music-challange

--- src/assets/js/fetchSpreadSheets.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export default function fetchSpreadSheets () {
-  return axios.get(`https://sheets.googleapis.com/v4/spreadsheets/${process.env.GRIDSOME_GOOGLE_SHEET_ID}?key=${process.env.GRIDSOME_GOOGLE_KEY}`).then(res => {
-    return res.data.sheets.map(sheet => {
-      return sheet.properties.title
-    })
-  })
-}
