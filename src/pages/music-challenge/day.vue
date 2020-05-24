ba4a13f15b21703abad8af77d2a6ef5ccb314eb8
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-05-24 20:46:31 +0200

    Add day loading

--- src/pages/music-challenge/day.vue
+++ src/pages/music-challenge/day.vue
@@ -1,5 +1,8 @@
 <template lang="html">
-  <div>
+  <div v-if="isLoading">
+    Loading...
+  </div>
+  <div v-else>
     <h2>{{ title }}</h2>
     <hr>
     <AddSong @songAdded="songAdded"/>
@@ -21,7 +24,8 @@ export default {
   },
   data () {
     return {
-      title: 'Loading...',
+      isLoading: true,
+      title: '',
       songs: []
     }
   },
@@ -32,16 +36,23 @@ export default {
 
     const inRange = (min, nr, max) => nr >= min && nr <= max
     const nr = window.parseFloat(this.$route.query.nr)
+
     if (!inRange(1, nr, 30) || nr !== Math.floor(nr)) {
+      this.$router.replace('/music-challenge/')
+      return
+    }
+
+    const { availableDays } = await axios.get('/available-days').then(res => res.data)
+
+    if (nr > availableDays) {
       this.$router.replace('/music-challenge/')
       return
     }
 
     const { data } = await axios.get(`/songs/${ nr }`)
     this.songs = data
-
-
     this.title = daysInfo[nr]
+    this.isLoading = false
   },
   methods: {
     songAdded (song) {
