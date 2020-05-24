67e8fd1442171dd5575194b6924d3df4c7595b7a
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-05-24 20:46:14 +0200

    Show available days

--- src/pages/music-challenge/Index.vue
+++ src/pages/music-challenge/Index.vue
@@ -1,17 +1,31 @@
 <template lang="html">
   <div>
-    c
+    <div v-for="i in availableDays">
+      <router-link :to="`/music-challenge/day?nr=${i}`">
+        {{ daysInfo[i] }}
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import daysInfo from '@/assets/js/daysInfo'
 
 export default {
+  data () {
+    return {
+      availableDays: 0,
+      daysInfo
+    }
+  },
   mounted () {
+    axios.get('/available-days').then(res => {
+      this.availableDays = res.data.availableDays
+    })
   }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
