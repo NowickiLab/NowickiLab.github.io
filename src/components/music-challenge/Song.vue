fbb7c3c95149b6aab1411a21f94358f57d528316
nidomika <dominika1872@gmail.com>
2020-06-06 01:43:44 +0200

    Create component

--- src/components/music-challenge/Song.vue
+++ src/components/music-challenge/Song.vue
@@ -4,19 +4,7 @@
     <div class="name">Sender: <b>{{ song.creator }}</b> ({{ date }})</div>
     <p class="description">{{ song.description }}</p>
 
-    <div class="like-wrap">
-      <span class="display-likes">
-        {{ displayLikes }}
-      </span>
-      <button class="music-challenge-btn like" @click="likeSong">
-        <svg class="heart" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-          <path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/>
-        </svg>
-      <span class="like-text">
-        {{ isLiked ? 'Liked!' : 'Like' }}
-      </span>
-      </button>
-    </div>
+    <Like :song="song"/>
     <iframe
       v-if="videoId"
       class="video"
@@ -36,6 +24,7 @@
 <script>
 import Comments from './Comments'
 import AddComment from './AddComment'
+import Like from './Like'
 import fetchToken from '@/assets/js/fetchRecaptchaToken'
 import axios from '@/assets/js/music-axios'
 
@@ -43,48 +32,23 @@ export default {
   props: ['song'],
   components: {
     Comments,
-    AddComment
+    AddComment,
+    Like
   },
   data () {
     return {
-      showIframe: false,
-      isLikesLoading: false,
-      isLiked: JSON.parse(localStorage.getItem('likedSongs')).includes(this.song.id),
+      showIframe: false
     }
   },
   computed: {
     videoId () {
       return (this.song.link.match(/v=([^&|$|#]+)/) || [])[1]
     },
-    displayLikes () {
-      const ppl = this.song.likes === 1 ? 'person likes' : 'people like'
-      if (this.song.likes) return `${this.song.likes} ${ppl} this`
-      return `No one has liked this yet!`
-    },
     date () {
       return new Date(this.song.date).toLocaleString()
     }
   },
   methods: {
-    likeSong () {
-      if (this.isLikesLoading) return
-      this.isLikesLoading = true
-
-      const likedSongs = JSON.parse(localStorage.getItem('likedSongs'))
-      if (likedSongs.includes(this.song.id)) return
-      likedSongs.push(this.song.id)
-      localStorage.setItem('likedSongs', JSON.stringify(likedSongs))
-      this.isLiked = true
-
-      fetchToken().then(token => {
-        return axios.post('/likes', { songId: this.song.id, token })
-      }).then(res => {
-        const likesNumber = res.data.likes
-        this.song.likes = likesNumber
-      }).finally(() => {
-        this.isLikesLoading = false
-      })
-    },
     onComment (comment) {
       this.song.comments.push(comment)
     }
@@ -97,7 +61,6 @@ export default {
 .song {
   border-top: 1px solid #ccc;
   margin-top: 30px;
-  position: relative;
 }
 
 .title {
@@ -121,49 +84,5 @@ export default {
   height: 325px;
   border-radius: 4px;
   margin: 12px 0 8px;
-}
-
-.like-wrap {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  // @include mq($from: tablet) {  
-  // display: flex;
-  // align-content: center;
-  // flex-direction: column;
-  // position: absolute;
-  // top: 20px;
-  // left: -70px;
-  // }
-}
-
-.display-likes {
-  align-self: center;
-}
-
-.like {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 136px;
-  fill: white;
-  background: #e44747;
-  border-color: #bb2e2e;
-
-  &:hover {
-    background: #bb2e2e;
-  }
-
-  // @include mq($from: tablet) {
-  //   border-radius: 50%;
-  //   width: 45px;
-  //   height: 45px;
-  // }
-}
-
-.heart {
-  height: 24px;
-  width: 24px;
-  margin-right: 8px;
 }
 </style>
