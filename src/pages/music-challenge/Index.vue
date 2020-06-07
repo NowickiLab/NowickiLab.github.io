5698e4f45ab5b4095a4019e40cbdeef60f78a4af
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2020-06-07 23:54:28 +0200

    Fix typos

--- src/pages/music-challenge/Index.vue
+++ src/pages/music-challenge/Index.vue
@@ -16,12 +16,12 @@
       <li>Posting for each day takes place between <b>00:00 AM</b> and <b>23:59 PM</b> (Memphis time).</li>
       <li><b>Stories</b> that are related to the song/music you post are encouraged.</li>
       <!-- <li>Past daily challenges (topics) are available at <a href="/music-challenge/">nowickilab.science/music-challenge</a></li> -->
-      <li>Likes and comments on the current and past daily topics are also encouraged. Liking and commenting is not limited to each day - let's enjoy enjoy the music any time we feel like it!</li>
+      <li>Likes and comments on the current and past daily topics are also encouraged. Liking and commenting is not limited to each day - let's enjoy the music any time we feel like it!</li>
       <!-- <li>No limits on likes/comments on others' music and stories!</li> -->
       <li>Please post <b>one</b> (max. two) songs for each day.</li>
       <li>Post carefully! If anyone needs to change/remove their song or story, please email me.</li>
       <li>Let's <b>keep it clean!</b> This is somewhat work-related activity, and as such we should have the maximum possible fun while keeping it civil. There will be moderation implemented for inappropriate comments/songs.</li>
-      <li>You can post your song under any name you wish. Can be your actual real life name, but does not have to be.</li>
+      <li>You can post your song under any name you wish. Can be your actual real-life name, but does not have to be.</li>
       <li>Likes (admiration of others) and <b>bringing joy</b> into other peoples' lives is the real price here. We do not anticipate any tangible awards.</li>
       <!-- <li>The website works both on computers and cellphones. Some experience may vary.</li> -->
     </ol>
